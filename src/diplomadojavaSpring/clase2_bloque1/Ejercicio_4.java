8277f7bb694059520ce97bc0cc2360fb7980e992
nicolas medina <niemep@gmail.com>
2024-10-15 21:36:28 -0300

    agrega ejercicios 4 y 5 bloque 1 clase 2

--- src/diplomadojavaSpring/clase2_bloque1/Ejercicio_4.java
+++ src/diplomadojavaSpring/clase2_bloque1/Ejercicio_4.java
@@ -1,10 +1,38 @@
 package diplomadojavaSpring.clase2_bloque1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio_4 {
 
 	public static void main(String[] args) {
 		// menor valor de un arreglo de enteros
+		
 
+		System.out.println("Ingrese largo del arreglo:");
+		Scanner scanner = new Scanner(System.in);
+		
+		int largo = scanner.nextInt();
+		
+		int[] nombre = new int[largo];
+		
+		
+		int mayor = 0;
+		
+		for(int i =0; i < largo; i++) {
+			Random aleatorio = new Random();
+			nombre[i] = aleatorio.nextInt();
+			System.out.println(nombre[i] + ", ");
+			
+			if(i == 0 ) {
+				mayor = nombre[i];
+			}
+			if(mayor > nombre[i]) {
+				mayor = nombre[i];
+			}
+		}
+		
+		System.out.println("menor:" + mayor);
 	}
 
 }
--- src/diplomadojavaSpring/clase2_bloque1/Ejercicio_5.java
+++ src/diplomadojavaSpring/clase2_bloque1/Ejercicio_5.java
@@ -1,9 +1,46 @@
 package diplomadojavaSpring.clase2_bloque1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ejercicio_5 {
 
 	public static void main(String[] args) {
 		// rango de un arreglo de enteros
+		
+		System.out.println("Ingrese largo del arreglo:");
+		Scanner scanner = new Scanner(System.in);
+		
+		int largo = scanner.nextInt();
+		
+		int[] nombre = new int[largo];
+		
+		
+		int mayor = 0;
+		int menor = 0;
+		
+		for(int i =0; i < largo; i++) {
+			Random aleatorio = new Random();
+			nombre[i] = aleatorio.nextInt();
+			System.out.println(nombre[i] + ", ");
+			
+			if(i == 0 ) {
+				mayor = nombre[i];
+				menor = nombre[i];
+			}
+			if(mayor < nombre[i]) {
+				mayor = nombre[i];
+			}
+			if(menor > nombre[i]) {
+				menor = nombre[i];
+			}
+		}
+		
+		int rango = mayor - menor;
+		
+		System.out.println("Mayor:" + mayor);
+		System.out.println("Menor:" + menor);
+		System.out.println("Rango:" + rango);
 
 	}
 
